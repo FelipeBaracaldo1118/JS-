11f13ad26069239b75f2206839a799f9e4494d96
Baracaldo <fbaracaldo@ea.com>
2026-09-14 23:17:23 -0500

    creating the new set up

--- fundamentals2/exercise.js
+++ fundamentals2/exercise.js
@@ -1,27 +1,29 @@
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+};
 
 /* Write your code below. Good luck! 🙂 */
 
-let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
-let tips=[]
-let totals=[]
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
 
-for(let i=0; i<bills.length; i++){
-    tips.push(calcTip(bills[i]))
-    totals.push(bills[i]+tips[i])
-
-    
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
 }
-console.log(bills, tips, totals)
+console.log(bills, tips, totals);
 
 let calcAverage = (arr) => {
-    let sum = 0;
-    for (let i=0 ;  i<arr.length; i++){
-        sum +=arr[i]
-    }
-    return sum/arr.length
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
 };
 
-console.log(calcAverage([bills[0]]))
+console.log(calcAverage([bills[0]]));
+
+if (bills > 10) {
+  console.log("The bill is greater than 10");
+}
